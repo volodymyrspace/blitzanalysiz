f8c71e08b61a8ff99409a88013f52b6f93da7c23
Jylpah <jylpah@gmail.com>
2023-08-20 00:14:48 +0300

    build v3: 35% faster vs. v2

---
+++ R/build_v3.R
@@ -0,0 +1,102 @@
+## Load utils if not loaded
+# if (!exists("pkgs")) {
+#   source("R/init.R")
+# }
+
+stopifnot(!is.null(build.version) && build.version == 3)
+
+##   T  E  S  T       B  U  I  L  D     ############################
+
+test_build <- FALSE
+
+if (test_build) cat("\n## TEST BUILD     ####################\n\n")
+
+####################################################################
+
+
+## Parameters -------------------------------------------------------
+tic("Total rebuild time")
+
+if (is.null(updates.build)) {
+    if (build.latest_only) {
+        updates.build <- get_latest_update(preliminary)
+    } else {
+        updates.build <- get_updates(since = "8.0", preliminary)
+    }
+}
+
+updates.no_build <- NULL
+updates.build <- updates.build[!updates.build %in% updates.no_build]
+updates.build <- intersect(updates.build, updates.all)
+disable_rebuild.career <- FALSE
+
+## This is needed to override default "update" that is function in "stats" package
+update <- updates.build[[1]]
+
+tiers <- seq(1, 10)
+if (test_build) tiers <- seq(6, 7)
+if (!exists("stats.update_ver")) stats.update_ver <- ""
+
+## Main pages
+pl.main <- pl_make_main()
+pl_build(pl.main, update.latest, force = force.update)
+
+for (update in rev(updates.build)) {
+    tic(paste("Update", update))
+    message("Building update ", update)
+
+    # update
+    pl.update.main <- pl_make_update_main(update, career = TRUE)
+    pl_build(pl.update.main, update, force = force.update)
+
+    for (tierN in seq(10)) {
+        message("Building update ", update, " tier ", get_tier_roman(tierN))
+        tic(paste("Update", update, "tier", get_tier_roman(tierN)))
+        # Tier main
+        pl <- pl_make_tiers_main(update, tiers = tierN)
+        pl <- pl_merge(pl, pl_make_tank_list(update, tiers = tierN))
+        pl <- pl_merge(pl, pl_make_tiers_types(update, tiers = tierN))
+        pl <- pl_merge(pl, pl_make_tiers_premiums(update, tiers = tierN))
+        pl <- pl_requires_rebuild(pl, force = force.update, update = update)
+
+        if (length(pl) > 0) {
+            # add tank pages only after update test since some tanks always fail the requires_rebuild test
+            pl <- pl_merge(pl, pl_make_tanks(update, tiers = tierN))
+
+            cols <- c(
+                "account_id", "tank_id", "battles", "battles.career",
+                "region", "name", "tier", "nation", "type", "is_premium", "WR",
+                "rWR", "WR.tier.maxed", "battles.tier.maxed", "avg_dmg", "avg_kills",
+                "spot_rate", "hit_rate", "shots", "survival_rate", "battle_life_time"
+            )
+
+            stats.tier <- ds_load_ts_update(update,
+                tank_tier = tierN,
+                cols = cols
+            )
+            stats.tier.perf <- get_stats_tank_perf(stats.tier)
+
+            pl_build_auto(pl, update,
+                build.type = build.type,
+                force = force.update,
+                sorted = TRUE,
+                n_cores = n_cores.default,
+                ex.vars = c("stats.tier", "stats.tier.perf")
+            )
+            rm("stats.tier.perf", "stats.tier")
+            gc(verbose = FALSE)
+        }
+        # players
+        pl_build(pl_make_tiers_players(update, tiers = tierN))
+
+        toc_()
+    } # for tier
+
+    # blog posts
+    pl.blog <- pl_make_blog_posts(update)
+    pl_build(pl.blog, update, force = force.update, sorted = TRUE)
+    toc_()
+} # for update
+
+dir.create(file.path("content", "tags"), showWarnings = FALSE)
+toc_()
--- R/init.R
+++ R/init.R
@@ -10,7 +10,7 @@ clear.stats_in_mem <- TRUE
 # build.type <- 'serial'    # nolint
 # build.type <- 'parallel'
 build.type <- "future"
-build.version <- 2
+build.version <- 3
 
 # setwd('C:/development/dev/blitzanalysiz')
 base_dir <- getwd()
@@ -20,43 +20,43 @@ base_dir <- getwd()
 OS <- Sys.info()[["sysname"]]
 
 if (OS == "Linux") {
-  options(rgl.useNULL = TRUE)
+    options(rgl.useNULL = TRUE)
 } else if (OS == "Windows") {
-  #
+    #
 } else {
-  warning(paste("Unknown OS:", OS))
+    warning(paste("Unknown OS:", OS))
 }
 ## Future
 options(
-  encoding = "UTF-8",
-  future.globals.maxSize = 100e9,
-  future.fork.multithreading.enable = FALSE,
-  future.wait.timeout = 120,
-  future.rng.onMisuse = "ignore",
-  future.debug = FALSE
+    encoding = "UTF-8",
+    future.globals.maxSize = 100e9,
+    future.fork.multithreading.enable = FALSE,
+    future.wait.timeout = 120,
+    future.rng.onMisuse = "ignore",
+    future.debug = FALSE
 )
 
 ## Packages
 # old <- c('showtext', 'Cairo')
 
 pkgs.loop <- c(
-  "rlist", "bit64", "lubridate", "matrixStats",
-  "ggplot2", "ggtext", "ggrepel", "knitr", "blogdown", "foreach",
-  "markdown", "rmarkdown", "svglite", "systemfonts", "extrafont", "qs",
-  "tictoc", "stats", "graphics", "reshape2", "stringr", "gMOIP",
-  "future", "data.table", "arrow", "dplyr"
+    "rlist", "bit64", "lubridate", "matrixStats",
+    "ggplot2", "ggtext", "ggrepel", "knitr", "blogdown", "foreach",
+    "markdown", "rmarkdown", "svglite", "systemfonts", "extrafont", "qs",
+    "tictoc", "stats", "graphics", "reshape2", "stringr", "gMOIP",
+    "future", "data.table", "arrow", "dplyr"
 )
 
 pkgs <- c(
-  pkgs.loop, "doParallel", "Hmisc", "mongolite", "progress",
-  "RhpcBLASctl", "httr", "ndjson", "jsonlite"
+    pkgs.loop, "doParallel", "Hmisc", "mongolite", "progress",
+    "RhpcBLASctl", "httr", "ndjson", "jsonlite"
 )
 
 # font_import(paths = "/usr/share/fonts/",prompt = F)
 
 for (pkg in pkgs) {
-  eval(parse(text = paste("suppressPackageStartupMessages(library(", pkg, "))")))
-  print(paste("Loaded:", pkg, "Version:", packageVersion(pkg)))
+    eval(parse(text = paste("suppressPackageStartupMessages(library(", pkg, "))")))
+    print(paste("Loaded:", pkg, "Version:", packageVersion(pkg)))
 }
 
 # Load variables
@@ -72,23 +72,23 @@ set_threads(n_threads)
 
 # Mongo
 URI <- sprintf(
-  "mongodb://%s:%s@%s/?sockettimeoutms=3600000",
-  db_user, db_passwd, db_host
+    "mongodb://%s:%s@%s/?sockettimeoutms=3600000",
+    db_user, db_passwd, db_host
 )
 ssl_opts <- NULL
 if (db_tls) {
-  URI <- paste0(URI, "&tls=true")
-  if (OS == "Linux") {
-    ssl_opts <- ssl_options(
-      ca = db_ca, cert = db_cert, key = db_key,
-      allow_invalid_hostname = allow_invalid_hostname,
-      weak_cert_validation = weak_cert_validation
-    )
-  } else if (OS == "Windows") {
-    ssl_opts <- ssl_options()
-  } else {
-    warning(paste("Unknown OS:", OS))
-  }
+    URI <- paste0(URI, "&tls=true")
+    if (OS == "Linux") {
+        ssl_opts <- ssl_options(
+            ca = db_ca, cert = db_cert, key = db_key,
+            allow_invalid_hostname = allow_invalid_hostname,
+            weak_cert_validation = weak_cert_validation
+        )
+    } else if (OS == "Windows") {
+        ssl_opts <- ssl_options()
+    } else {
+        warning(paste("Unknown OS:", OS))
+    }
 }
 
 # WG_tank_stats <- mongo('WG_TankStats', 'BlitzStats', URI, verbose = TRUE, options = ssl_opts);
@@ -102,7 +102,7 @@ mongo_options(log_level = 2) # 2 = WARNING see ?mongo_options
 
 updates <- get_blitz_updates()
 updates.all <- get_updates(since = "6.0", preliminary)
-updates.build.updates <- get_updates(since = "8.0", preliminary)
+updates.build.updates <- get_updates(since = "9.5", preliminary)
 updates.build.blog <- updates.all
 update.first_with_stats <- "5.4"
 update.latest <- get_latest_update(preliminary)
